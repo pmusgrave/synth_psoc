21d4eb303ac2d891f90960b1b06d9602eb110ad1
Paul Musgrave <ps.musgrave@gmail.com>
2018-07-17 18:41:59 -0400

    hardware config already changed due to PSOC routing add init code for envelope generator potentiometers

--- synth_psoc.cydsn/envelope_generator.c
+++ synth_psoc.cydsn/envelope_generator.c
@@ -4,7 +4,15 @@
 #include "envelope_generator.h"
 
 void InitEnvelopeGenerator(){
-    
+    // attack_pot_high_Write(1);
+    attack_pot_low_Write(0);
+    decay_pot_high_Write(1);
+    decay_pot_low_Write(0);
+    sustain_pot_high_Write(1);
+    sustain_pot_low_Write(0);
+    release_pot_high_Write(1);
+    release_pot_low_Write(0);
 }
+
 
 /* [] END OF FILE */
