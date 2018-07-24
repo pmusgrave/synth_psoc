7b7d728bfc6c62d2ca258260d04faee58ca29064
Paul Musgrave <ps.musgrave@gmail.com>
2018-07-23 22:03:45 -0400

    starting to prototype LFO with red LED freq currently set by same knob as square wave oscillator

--- synth_psoc.cydsn/main.c
+++ synth_psoc.cydsn/main.c
@@ -76,7 +76,8 @@ int main(void)
     
     ADC_SAR_Seq_1_Start();
     ADC_SAR_Seq_1_StartConvert();
-    PWM_1_Start();
+    main_osc_PWM_Start();
+    LFO_PWM_Start();
     LED_PWM_Start();    
     ADC_EOC_INT_StartEx(ADC_EOC);
     ENVELOPE_TIMER_Start();
@@ -97,16 +98,20 @@ int main(void)
         
         if(pwm_update_flag != 0){ 
             pwm_update_flag = 0;
-            PWM_1_WritePeriod((uint16) 65535/freq);
+            main_osc_PWM_WritePeriod((uint16) 65535/freq);
             
             // this is a square wave with 50% duty cycle
-            //PWM_1_WriteCompare((uint16) (65535/freq)/2);
+            //main_osc_PWM_WriteCompare((uint16) (65535/freq)/2);
             
             // this is pulse width modulation, although the 2000 does not make any sense.
             // wasn't getting good results with 65535/pulse_width?
             // fix this later
-            PWM_1_WriteCompare((uint16) (65535/freq)/(2000/pulse_width));
+            main_osc_PWM_WriteCompare((uint16) (65535/freq)/(2000/pulse_width));
       
+            LFO_PWM_WritePeriod((uint16) 65535/freq);
+            LFO_PWM_WriteCompare((uint16) (65535/freq)/(2000/pulse_width));
+            
+            
             switch(current_mode){
             case NOT_TRIGGERED:
                 pwm_value = 0;
