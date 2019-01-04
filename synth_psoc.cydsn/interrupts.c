9faaf6b8671298c3b8639b0683d549d5f78fd9d6
Paul Musgrave <ps.musgrave@gmail.com>
2019-01-04 17:35:15 -0500

    move envelope output to inverted pwm driver for driving the gate of a P-channel FET

--- synth_psoc.cydsn/interrupts.c
+++ synth_psoc.cydsn/interrupts.c
@@ -10,13 +10,21 @@ CY_ISR(ADC_EOC) {
     pulse_width = ADC_SAR_Seq_1_GetResult16(2);  
     
     attack_pot_value = ADC_SAR_Seq_1_GetResult16(ATTACK_POT_ADC_CHANNEL);
+    if(attack_pot_value < 100){
+        attack_pot_value = 1;
+    }    
     decay_pot_value = ADC_SAR_Seq_1_GetResult16(DECAY_POT_ADC_CHANNEL);
+    if(decay_pot_value < 100){
+        decay_pot_value = 1;
+    }    
     sustain_pot_value = ADC_SAR_Seq_1_GetResult16(SUSTAIN_POT_ADC_CHANNEL);
     if(sustain_pot_value < 100){
-        sustain_pot_value = 0;
+        sustain_pot_value = 1;
     }    
     release_pot_value = ADC_SAR_Seq_1_GetResult16(RELEASE_POT_ADC_CHANNEL);
-    
+    if(release_pot_value < 100){
+        release_pot_value = 5;
+    }    
     ADC_EOC_INT_ClearPending();
 }
 
--- synth_psoc.cydsn/main.c
+++ synth_psoc.cydsn/main.c
@@ -54,8 +54,8 @@ int main(void)
                 release_debug_led_Write(0);
                 
                 pwm_value = 0;
-                ENVELOPE_TIMER_WriteCounter(0);
                 ENVELOPE_TIMER_Stop();
+                ENVELOPE_TIMER_WriteCounter(0);
                 break;
             case ATTACK_MODE:
                 attack_debug_led_Write(1);
@@ -67,6 +67,9 @@ int main(void)
                 ENVELOPE_TIMER_WritePeriod(attack_pot_value);
                 ENVELOPE_TIMER_WriteCompare(attack_pot_value);
                 pwm_value = (ENVELOPE_MAX_PERIOD / attack_pot_value) * ENVELOPE_TIMER_ReadCounter();
+                if(pwm_value >= ENVELOPE_MAX_PERIOD){
+                    current_mode++;
+                }
                 break;
             case DECAY_MODE:
                 attack_debug_led_Write(0);
@@ -77,15 +80,18 @@ int main(void)
                 ENVELOPE_TIMER_WritePeriod(decay_pot_value);
                 ENVELOPE_TIMER_WriteCompare(decay_pot_value);
                 pwm_value = (double)((sustain_pot_value - ENVELOPE_MAX_PERIOD)/ decay_pot_value) * ENVELOPE_TIMER_ReadCounter() + ENVELOPE_MAX_PERIOD;
+                if(pwm_value <= sustain_pot_value){
+                    current_mode++;
+                }
                 break;
             case SUSTAIN_MODE:
                 attack_debug_led_Write(0);
                 decay_debug_led_Write(0);
                 sustain_debug_led_Write(1);
                 release_debug_led_Write(0);
-                
-                //ENVELOPE_TIMER_WriteCounter(0);
+
                 ENVELOPE_TIMER_Stop();
+                //ENVELOPE_TIMER_WriteCounter(0);
                 pwm_value = sustain_pot_value;
                 break;
             case RELEASE_MODE:
@@ -94,11 +100,15 @@ int main(void)
                 sustain_debug_led_Write(0);
                 release_debug_led_Write(1);
                 
-                ENVELOPE_TIMER_Enable();
                 ENVELOPE_TIMER_WritePeriod(release_pot_value);
                 ENVELOPE_TIMER_WriteCompare(release_pot_value);
+                ENVELOPE_TIMER_Enable();
                 //pwm_value = (double)((-sustain_pot_value * ENVELOPE_TIMER_ReadCounter() / release_pot_value)) + ENVELOPE_MAX_PERIOD;
                 pwm_value = (double)((0- sustain_pot_value)/ release_pot_value) * ENVELOPE_TIMER_ReadCounter() + sustain_pot_value;
+                
+                if(pwm_value == 0){
+                    current_mode = NOT_TRIGGERED;
+                }
                 break;
             }
             
